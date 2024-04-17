04d44dc3621d03a80e099202f725062cb4293c8a
Abdelbar Terbachi <abdelbarterbachi@gmail.com>
2024-04-17 14:41:05 +0100

    fix filtering bugs

--- app/issues/list/IsuueStatusFilter.tsx
+++ app/issues/list/IsuueStatusFilter.tsx
@@ -2,7 +2,7 @@
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const status: { label: string; value: Status | "All" }[] = [
@@ -14,24 +14,31 @@ const status: { label: string; value: Status | "All" }[] = [
 
 const IsuueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
-  return (
-    <Select.Root
-      onValueChange={(status) => {
-        const query = status !== "All" ? `?status=${status}` : "";
-        router.push("/issues/list" + query);
-      }}
-    >
-      <Select.Trigger placeholder="Filter by status..." />
-      <Select.Content>
-        {status.map((stat) => (
-          <Select.Item key={stat.value} value={stat.value || ""}>
-            {stat.label}
-          </Select.Item>
-        ))}
-      </Select.Content>
-    </Select.Root>
-  );
+  if (status)
+    return (
+      <Select.Root
+        defaultValue={searchParams.get("status") || ""}
+        onValueChange={(status) => {
+          const params = new URLSearchParams();
+          if (status) params.append("status", status);
+          if (searchParams.get("orderBy"))
+            params.append("orderBy", searchParams.get("orderBy")!);
+          const query = params.size ? "?" + params.toString() : "";
+          router.push("/issues/list" + query);
+        }}
+      >
+        <Select.Trigger placeholder="Filter by status..." />
+        <Select.Content>
+          {status.map((stat) => (
+            <Select.Item key={stat.value} value={stat.value || ""}>
+              {stat.label}
+            </Select.Item>
+          ))}
+        </Select.Content>
+      </Select.Root>
+    );
 };
 
 export default IsuueStatusFilter;
